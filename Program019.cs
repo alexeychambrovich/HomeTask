c4ac8b28f981995466280d93bd165bacc5979a98
alexeychambrovich <114851932+alexeychambrovich@users.noreply.github.com>
2022-10-15 12:02:28 +0500

    Домашнее задание к уроку_3

---
+++ Program019.cs
@@ -0,0 +1,23 @@
+﻿// НАпишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет; 12821 -> да; 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int original = n;
+int reversed = 0;
+
+while (n != 0)
+{
+    int remaider = n % 10;
+    reversed = reversed * 10 + remaider;
+    n /= 10;
+}
+
+if (original == reversed)
+{
+    Console.WriteLine($"{original} - палиндром"); 
+}
+else
+{
+    Console.WriteLine($"{original} - не палиндром"); 
+}
---
+++ Program023.cs
@@ -0,0 +1,28 @@
+﻿// напишите пНапишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27;   5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.WriteLine("Введите N: ");
+
+bool correct = true;
+while (correct)
+{
+    try
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        List<int> array = new List<int>();
+
+        if (N > 0)
+        {
+            for (double i = 1; i <= N; i++)
+            {
+                array.Add(Convert.ToInt32(Math.Pow(i, 3)));
+            }
+        }
+        Console.Write(String.Join(", ", array));
+    }
+    catch (System.Exception)
+    {
+        
+        Console.WriteLine("Введено неверное значение");
+    }
+}
