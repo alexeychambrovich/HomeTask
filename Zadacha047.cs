ac5c8009d149e7f48fd00f357135851583144bf7
Alexey Chambrovich <114851932+alexeychambrovich@users.noreply.github.com>
2022-11-10 21:51:32 +0500

    Домашнее задание к уроку_7

---
+++ Zadacha047.cs
@@ -0,0 +1,35 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите высоту матрицы:  ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите длину матрицы:   ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m,n];
+Random rand = new Random();
+
+void FillArray(double[,] matr){
+    for (int i = 0; i < matr.GetLength(0); i++){
+        for (int j = 0; j < matr.GetLength(1); j++){
+            //matr[i,j] = new Random().Next(1, 10);
+            matr[i,j] = rand.NextDouble();
+        }
+    }
+
+}
+
+void Print(double[,] matr){
+    for (int i = 0; i < matr.GetLength(0); i++){
+       for (int j = 0; j < matr.GetLength(1); j++){ 
+            Console.Write(matr[i,j] + "  ");
+    }
+    Console.WriteLine();
+}
+}
+
+FillArray(array);
+Print(array);
---
+++ Zadacha052.cs
@@ -0,0 +1,35 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void Main(string[] args){
+    double[,] a = new double[5,5];
+    double[] b = new double[5];
+
+    for (int i = 0; i < 5; i++){
+       for (int j = 0; j < 5; j++){
+        a[i,j] = i*j+1;
+        Console.Write(a[i,j] + "  ");
+       } 
+       Console.WriteLine();
+    }
+
+    for (int i = 0, k = 0; i < 5; i++){
+        double sum = 0;
+        for (int j = 0; j < 5; j++)
+        {
+            sum += a[i,j];
+        }
+        b[k] = sum/5;
+        // Console.WriteLine("Среднее арифметическое столбца "+(i)+"  равно: "+b[k]);   
+        Console.WriteLine("Среднее арифметическое столбца "+(i+1)+"  равно: "+b[k]);
+        k++;
+    }
+        Console.WriteLine();
+
+}
+Main(args);
+
